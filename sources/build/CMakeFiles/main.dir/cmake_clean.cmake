8c381812f63c5c0d2e8b0377cbf079a4d5c99e1e
sysu_hpcedu_302 <i@wu-kan.cn>
2020-07-16 16:24:20 +0800

    baseline

---
+++ sources/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main_generated_core.cu.o"
+  "CMakeFiles/main.dir/src/main_generated_main.cu.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
